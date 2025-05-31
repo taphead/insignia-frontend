fb3da7e523860670d4aec9f8caa4d3c71a189c55
taphead <techne.trax@proton.me>
2025-05-31 12:45:12 +0600

    added footer to testimonial

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -2,6 +2,9 @@ import TestimonialCard from "./TestimonialCard";
 import "./TestimonialSection.css";
 import avatar1 from "../assets/john.png";
 import avatar2 from "../assets/jenny.png";
+import purplecircle from "../assets/purplecircle.png";
+import whitecircle from "../assets/whitecircle.png";
+import LeftArrow from "./LeftArrow";
 
 export default function TestimonialSection() {
   return (
@@ -26,8 +29,16 @@ Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar ae
         />
       </div>
       <div className="testimonialfooter">
-        <div>Footer</div>
-        <div>Right</div>
+        <div className="testimonialcircles">
+          <img src={purplecircle} className="testimonialcirclepurple" />
+          <img src={whitecircle} className="testimonialcirclewhite" />
+          <img src={whitecircle} className="testimonialcirclewhite" />
+          <img src={whitecircle} className="testimonialcirclewhite" />
+        </div>
+        <div className="testimonialarrows">
+          <LeftArrow style="normal" />
+          <LeftArrow style="inverted" />
+        </div>
       </div>
     </div>
   );
